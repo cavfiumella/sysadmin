75b3d2a51080b7c95c6b780b3c343e6eea9a5e45
Andrea Serpolla <a.serpolla@cavfiumella.it>
2022-06-12 13:43:57 +0200

    Cli args for stack_healthcheck.sh
    Add help.

--- bin/stack_healthcheck.sh
+++ bin/stack_healthcheck.sh
@@ -1,7 +1,19 @@
 #!/bin/bash
-#
-# Check docker stacks' services are all running
-#
+
+print_help() {
+	echo 'Check Docker stacks services running state.'
+	echo
+	echo 'Usage:'
+	echo "  `basename $BASH_SOURCE` [-h|--help] [-D|--dry-run] <stack>"
+	echo
+	echo 'Args:'
+	echo '  stack          Docker stack to check'
+	echo
+	echo 'Options:'
+	echo '  -h --help      print usage and help'
+
+	return 0
+}
 
 
 # check stack exists
@@ -35,28 +47,50 @@ is_running() {
 
 main() {
 
-	stacks="$@"
+	args=()
 
-	for stack in "$stacks"; do
-		exists "$stack"
-		if [[ $? -ne 0 ]]; then
-			echo Stack $stack not found! 1>&2
-			return 1
-		fi
+	while [[ $# -gt 0 ]]; do
+		case $1 in
+			-h|--help)
+				print_help
+				return 0
+				;;
+			-*|--*)
+				echo Unknown option $1. 1>&2
+				echo
+				print_help
+				return 1
+				;;
+			*)
+				args+=("$1")
+				shift
+				;;
+		esac
 	done
 
-	for stack in "$stacks"; do
-		is_running "$stack"
-		if [[ $? -ne 0 ]]; then
-			echo Stack $stack is not running! 1>&2
-			echo Stack services: 1>&2
-			docker stack services "$stack" 1>&2
+	if [[ ${#args[@]} -lt 1 ]]; then
+		echo Stack name is needed!
+		return 1
+	fi
 
-			return 1
-		fi
-	done
+	stack="${args[0]}"
+
+	exists "$stack"
+	if [[ $? -ne 0 ]]; then
+		echo Stack $stack does not exist! 1>&2
+		return 1
+	fi
+
+	is_running "$stack"
+	if [[ $? -ne 0 ]]; then
+		echo Some service is not running! 1>&2
+		echo Stack $stack services: 1>&2
+		docker stack services "$stack" 1>&2
+
+		return 1
+	fi
 	
-	echo All stacks are running.
+	echo All services are running.
 	return 0
 }
 
