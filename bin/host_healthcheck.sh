bee457288d882d99f9c193258941b2c75cc8c124
Andrea Serpolla <a.serpolla@cavfiumella.it>
2022-06-28 11:56:26 +0200

    Ping hosts
    Hosts healthcheck using pings.

---
+++ bin/host_healthcheck.sh
@@ -0,0 +1,59 @@
+#!/bin/bash
+
+print_help() {
+	echo 'Ping host to check is alive.'
+	echo
+	echo "Usage:  $SOURCE [options] <ping target>"
+	echo
+	echo 'Arguments:'
+	echo '  target         ping target'
+	echo
+	echo 'Options:'
+	echo '  -h | --help    print usage and help'
+
+	return 0
+}
+
+
+main() {
+	args=()
+
+	while [ $# -gt 0 ]; do
+		case "$1" in
+			-h | --help)
+				print_help
+				return 0
+				;;
+			-* | --*)
+				echo "Unrecognized option $1!" 1>&2
+				print_help
+				return 1
+				;;
+			*)
+				args+=("$1")
+				shift
+				;;
+		esac
+	done
+
+	if [ ${#args[@]} -lt 1 ]; then
+		echo 'Ping target is mandatory!' 1>&2
+		return 1
+	fi
+
+	returncode=0
+
+	for host in "$args"; do
+		ping -c1 "$host" &>/dev/null
+		
+		if [ $? -ne 0 ]; then
+			echo "$host is down!" 1>&2
+			returncode=1
+		fi
+	done
+
+	return $returncode
+}
+
+main "$@"
+exit $?
